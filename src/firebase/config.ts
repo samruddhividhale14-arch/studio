7224b7d6f44e353fe775a2f6c2d1753d4ae83514
Samruddhi Vidhale <samruddhividhale14@gmail.com>
2025-11-01 08:47:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3996274845-fbd2b",
+  "appId": "1:88924493559:web:de721624b4037249d4e5f2",
+  "apiKey": "AIzaSyABS55BLPp-vOMCTaQiVH6loVrlvo9S_JM",
+  "authDomain": "studio-3996274845-fbd2b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "88924493559"
+};
